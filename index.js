37d4dd4354e2a82a8b0b5ad7d0cc347f48c7e700
serico7 <srconley@pacbell.net>
2019-11-16 19:18:32 -0800

    index simpler

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const week09 = require('./script/week09.js');
-const STATIC_DIR = path.join(__dirname, 'public')
-const VIEWS_DIR = path.join(__dirname, 'views');
 
-express()
-  .use(express.static(STATIC_DIR))
-  .set('views', VIEWS_DIR)
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => { res.sendfile(`${STATIC_DIR}/index.html`); })
-  .get('/getRate', (req, res) => { week09.getRate(req, res) })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var app = express()
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => { res.sendfile(`${STATIC_DIR}/index.html`); })
+app.get('/getRate', (req, res) => { week09.getRate(req, res) })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
